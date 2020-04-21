c74020572b795d0c0183623bb8e1e1f4d7ee2d73
Anas ESSAHL <ESSAHL.Anas@ofppt-edu.ma>
2020-04-21 22:28:22 +0000

    Register page Design 80%

---
+++ PFM/PFM/Scripts/MyScripts/DisplayPassword.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    var clicked = false, clicked2 = false;
+    $(".icon-con").click(function () {
+        if (!clicked) {
+            $(".inputCon").attr("type", "text");
+            $(".icon-con").css("background-image", 'url(../pic/eye-slash-solid.svg)');
+            clicked = true;
+        }
+        else if (clicked) {
+            $(".inputCon").attr("type", "Password");
+            $(".icon-con").css("background-image", 'url(../pic/eye-solid.svg)');
+            clicked = false;
+        }
+    });
+    $(".icon-pass").click(function () {
+        if (!clicked2) {
+            $(".passinput").attr("type", "text");
+            $(".icon-pass").css("background-image", 'url(../pic/eye-slash-solid.svg)');
+            clicked2 = true;
+        }
+        else if (clicked2) {
+            $(".passinput").attr("type", "Password");
+            $(".icon-pass").css("background-image", 'url(../pic/eye-solid.svg)');
+            clicked2 = false;
+        }
+    });
+});
